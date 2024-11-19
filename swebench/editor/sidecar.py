8355d2e81dc31747ab0f00e04917206d08a68485
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-11-19 13:42:53 +0000

    Revert "[swe_bench] chore: remove venv"
    This reverts commit 190f5baae4afb884b94c5e4f56d6ac8d17af095f.

---
+++ swebench/editor/sidecar.py
@@ -0,0 +1,64 @@
+import json
+import os
+import tempfile
+import subprocess
+from swebench.harness.constants import SWEbenchInstance
+
+
+def sidecar_run(
+    sidecar_path: str,
+    git_drname: str,
+    endpoint_url: str,
+    instance: SWEbenchInstance,
+    run_id: str,
+):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmp_path = tmpdirname
+
+        command_args = [
+            sidecar_path,
+        ]
+        json_data = {
+            "git_drname": git_drname,
+            "instance": instance,
+        }
+        temp_file_path = os.path.join(tmp_path, 'data.json')
+        with open(temp_file_path, 'w') as temp_file:
+            json.dump(json_data, temp_file)
+
+        command_args.append("--input")
+        command_args.append(temp_file_path)
+        command_args.append("--timeout")
+        command_args.append("1800")
+        command_args.append("--editor-url")
+        command_args.append(endpoint_url)
+
+        command_args.append("--run-id")
+        command_args.append(run_id)
+
+        command_args.append("--anthropic-api-key")
+        command_args.append("")
+        print("sidecar_binary_args", command_args)
+
+        process = subprocess.Popen(
+            command_args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,  # Ensures output is decoded as text (str) instead of bytes
+        )
+
+        # Stream and print stdout and stderr in real-time
+        try:
+            for line in process.stdout:
+                print(f"STDOUT: {line.strip()}")
+            for line in process.stderr:
+                print(f"STDERR: {line.strip()}")
+        except Exception as e:
+            print(f"Error while streaming process output: {e}")
+
+        # Wait for process to complete
+        process.wait()
+
+        # Check the exit code
+        if process.returncode != 0:
+            raise RuntimeError(f"Sidecar process failed with return code {process.returncode}")
