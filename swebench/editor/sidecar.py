c6c4b907be2c12f92082646db18840a99d209862
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-11-19 16:02:07 +0000

    [swe_bench] parea link

--- swebench/editor/sidecar.py
+++ swebench/editor/sidecar.py
@@ -36,6 +36,10 @@ async def sidecar_run(
         ])
         print("sidecar_binary_args", command_args)
 
+        link = f"""https://app.parea.ai/logs?colViz=%7B%220%22%3Afalse%2C%221%22%3Afalse%2C%222%22%3Afalse%2C%223%22%3Afalse%2C%22error%22%3Afalse%2C%22deployment_id%22%3Afalse%2C%22feedback_score%22%3Afalse%2C%22time_to_first_token%22%3Afalse%2C%22scores%22%3Afalse%2C%22start_timestamp%22%3Afalse%2C%22user%22%3Afalse%2C%22session_id%22%3Afalse%2C%22target%22%3Afalse%2C%22experiment_uuid%22%3Afalse%2C%22dataset_references%22%3Afalse%2C%22in_dataset%22%3Afalse%2C%22event_type%22%3Afalse%2C%22request_type%22%3Afalse%2C%22evaluation_metric_names%22%3Afalse%2C%22request%22%3Afalse%2C%22calling_node%22%3Afalse%2C%22edges%22%3Afalse%2C%22metadata_evaluation_metric_names%22%3Afalse%2C%22metadata_event_type%22%3Afalse%2C%22metadata_0%22%3Afalse%2C%22metadata_calling_node%22%3Afalse%2C%22metadata_edges%22%3Afalse%2C%22metadata_root_id%22%3Afalse%7D&filter=%7B%22filter_field%22%3A%22meta_data%22%2C%22filter_operator%22%3A%22equals%22%2C%22filter_key%22%3A%22%22%2C%22filter_value%22%3A%22{run_id}%22%7D&page=1"""
+
+        print(f"PAREA LINK:\n===\n{link}\n===")
+
         process = await asyncio.create_subprocess_exec(
             *command_args,
             stdout=asyncio.subprocess.PIPE,
