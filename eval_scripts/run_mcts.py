30ea02b97cc8fb5acccf0b0e5c7bc3a9aa56a574
skcd <sandeepkumarpani888@gmail.com>
2024-12-09 11:27:31 +0000

    [swe-bench] do a single traj

--- eval_scripts/run_mcts.py
+++ eval_scripts/run_mcts.py
@@ -41,7 +41,7 @@ async def run_command_for_instance(instance_id, anthropic_api_key, sidecar_binar
             f"--anthropic_api_key {anthropic_api_key} " # don't forget to preserve the trailing space
             f"--run_id {run_id} "
             f"--output_log_path {output_log_path} "
-            f"--traj_search_space 2" # allow for upto 2 trajectories to be generated, trying to exhaust the search space
+            f"--traj_search_space 1" # allow for upto 2 trajectories to be generated, trying to exhaust the search space
         )
 
         print(run_command)
